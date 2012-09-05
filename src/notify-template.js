7f4d616ebad3b8f4a6c2fba8665df4211d829d85
zeekay <zeekayy@gmail.com>
2012-09-04 18:41:52 -0700

    All good but templating.

---
+++ src/notify-template.js
@@ -0,0 +1,41 @@
+<div class="indicator i<#= account.id % 4 #>">
+   <div id="inbox_<#= account.id #>" class="vbox rounded">
+      <div class="vbox" id="<#= mail.id #>">
+         <div class="hbox address padded">
+            <div><a class="inboxLink" title="<#= account.getInboxLink() #>"><#= account.getAddress() #></a></div>
+         </div>
+
+         <div class="hbox sender">
+            <div class="hbox wide padded">
+               <a class="author" href="mailto:<#= mail.authorMail  #>" title="<#= mail.authorMail  #>"><#= mail.authorName  #></a>
+            </div>
+            <div class="hbox padded">
+               <label><#= formatDateTime(mail.issued, i18n.selected_lang.months) #></label>
+            </div>
+         </div>
+
+         <div class="hbox title">
+            <div class="hbox wide padded">
+               <a class="starLink imageLink" mailId="<#= mail.id #>" href="#" title="<#= i18n.get('starLinkTitle') #>"><img src="img/star.png" /></a>
+               <a class="openLink" mailId="<#= mail.id #>" href="#" title="<#= mail.title #>"><#= mail.shortTitle #></a>
+            </div>
+         </div>
+
+         <div class="hbox">
+            <div class="hbox wide border-top padded summary" mailId="<#= mail.id #>">
+               <#= mail.summary #>
+            </div>
+         </div>
+
+         <div class="hbox padded hiddenSummaryActions">
+            <a class="readLink" mailId="<#= mail.id #>" title="<#= i18n.get('readLinkTitle') #>"><#= i18n.get('readLink') #></a>
+            &nbsp;
+            <a class="deleteLink" mailId="<#= mail.id #>" title="<#= i18n.get('deleteLinkTitle') #>"><#= i18n.get('deleteLink') #></a>
+            &nbsp;
+            <a class="spamLink" mailId="<#= mail.id #>" title="<#= i18n.get('spamLinkTitle') #>"><#= i18n.get('spamLink') #></a>
+            &nbsp;
+            <a class="archiveLink" mailId="<#= mail.id #>" title="<#= i18n.get('archiveLinkTitle') #>"><#= i18n.get('archiveLink') #></a>
+         </div>
+      </div>
+   </div>
+</div>
